4b05c2f70e9f2ca62939d6a811f0e75e7f76eccf
[julianlorenteiesnervion] <[julian.lorente@iesnervion.es]>
2025-01-31 10:59:41 +0100

    Código modificado por Julián

--- ProyectoEntornosJulianJoseManuel/EntornosDD/src/EjerJulianJose.java
+++ ProyectoEntornosJulianJoseManuel/EntornosDD/src/EjerJulianJose.java
@@ -3,8 +3,9 @@ import java.util.*;
 public class EjerJulianJose {
 
 	public static void main(String[] args) {
+		// For que dice los números pares / impares del 1 al 10
 		for (int i = 1; i <= 10; i++)
-			System.out.print(i + " ");
+			System.out.println(i + (i % 2 == 0 ? " es par" : " no es par"));
 	}
 
 }
