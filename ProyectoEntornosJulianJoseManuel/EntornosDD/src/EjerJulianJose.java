336d1da4dd422624f3cd5311fc7424443b0c7d27
[julianlorenteiesnervion] <[julian.lorente@iesnervion.es]>
2025-02-07 10:53:20 +0100

    Cambios realizados.

--- ProyectoEntornosJulianJoseManuel/EntornosDD/src/EjerJulianJose.java
+++ ProyectoEntornosJulianJoseManuel/EntornosDD/src/EjerJulianJose.java
@@ -3,6 +3,9 @@ import java.util.*;
 public class EjerJulianJose {
 
 	public static void main(String[] args) {
+		// Mensaje de bienvenida
+		System.out.println("Hola, bienvenido");
+		
 		// For que dice los números pares / impares del 1 al 10
 		for (int i = 1; i <= 10; i++) {
 			System.out.println(i + (i % 2 == 0 ? " es par" : " no es par"));System.out.println();
